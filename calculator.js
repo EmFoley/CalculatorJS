9e7424ae6fb08045230e47ddacc53b87542389f7
Emily Foley <foleyemilym@gmail.com>
2014-06-11 17:05:32 -0700

    adding js logic for displaying values in results window

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// on click of button, add value to value array and display
+// in results window
+// on click of operator, operate
+
+// repeat until = is hit, then display final result 
+
+// C once clears the current value array, C twice clears all the arrays
+
+function collectValues() {
+ 
+
+};
+
+var clickButton = getElementById('num');
+clickButton.addEventListener("click", collectValues, false);
