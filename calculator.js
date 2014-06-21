5de4ed57f283ef7e38309dd583efcf87ec2df894
Emily Foley <foleyemilym@gmail.com>
2014-06-20 17:05:58 -0700

    debugging 'this'

--- calculator.js
+++ calculator.js
@@ -1,17 +1,16 @@
-// on click of button, add value to value array and display
-// in results window
-// on click of operator, operate
 
-// repeat until = is hit, then display final result 
-
-// C once clears the current value array, C twice clears all the arrays
-
-function collectValues() {
- var enteredIn = [];
- var valEntered = document.getElementById
- enteredIn.push(value);
- console.log(enteredIn);
+var collectValues = function() {
+   var enteredIn = [];
+   var valEntered = this.val;
+   debugger
+   // enteredIn.push(valEntered);
+   console.log(valEntered);
 };
 
-var key = document.getElementById('keys');
-key.addEventListener("click", collectValues, false);
+var keys = document.getElementById('keys');
+var buttons = keys.childNodes;
+
+for (var i=0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.addEventListener("click", collectValues, false);
+};
