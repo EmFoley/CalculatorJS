e33eebec4b1bded0d14ae4d26eebde834153bc9a
Emily Foley <foleyemilym@gmail.com>
2014-07-02 16:06:10 -0700

    holding

--- calculator.js
+++ calculator.js
@@ -2,7 +2,6 @@
 var collectValues = function() {
    var enteredIn = [];
    var valEntered = this.val;
-   debugger
    enteredIn.push(valEntered);
    console.log(valEntered);
 };
@@ -10,7 +9,7 @@ var collectValues = function() {
 var keys = document.getElementById('keys');
 var buttons = keys.childNodes;
 
-for (var i=0; i < buttons.length; i++ {
+for (var i=0; i < buttons.length; i++) {
   var button = buttons[i];
   button.addEventListener("click", collectValues, false);
-});
+};
