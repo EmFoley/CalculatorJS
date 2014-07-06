f481286e85eaf383b40fae2636dda7efbc308b4f
Emily Foley <foleyemilym@gmail.com>
2014-07-05 18:06:52 -0700

    equals event listener

--- calculator.js
+++ calculator.js
@@ -3,16 +3,22 @@
 var collectValues = function() {   
    var valEntered = this.value;
    enteredIn.push(valEntered);
-   evalArray(enteredIn.join(''));
+   showArray(enteredIn.join(''));
 };
 
 var result = document.getElementById("result-view");
-var evalArray = function(numsEntered) {
+var showArray = function(numsEntered) {
   result.innerHTML = numsEntered;
 };
 
-var evaluateNow = function() {
+var equals = document.getElementsByClassName("equals")
+var equals = function() {
+  equals.addEventListener("click", evaluateNow, false)
+};
 
+var evaluateNow = function() {
+  if evalArray
+   showArray(evalArray);
 };
 
 var buttons = document.getElementsByClassName('button');
